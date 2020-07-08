2242cb64c1d827d5e96dec439512cbc683ceb540
Victor Fernandes <victorfernandes.matias@gmail.com>
2020-07-08 02:17:35 -0300

    Adição do metodo get_pagina que conecta a pagina desejada e envia um post através do formulário de busca baseado no valor informado através da CLI.

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from argparse import ArgumentParser
+import requests
+from bs4 import BeautifulSoup
 
 
-parser = ArgumentParser()
-parser.add_argument('query', help='Informe a query desejada')
-
-arg = parser.parse_args()
-
+def get_pagina(query):
+    data = requests.post('https://wiki.archlinux.org/', data={'search': query})
+    return data
 
 
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('query', help='Informe a query desejada')
 
+    arg = parser.parse_args()
 
-
-
-
-
-
-
-
-
+    html_soup = BeautifulSoup(get_pagina(arg.query).text, 'html.parser')
 
 
